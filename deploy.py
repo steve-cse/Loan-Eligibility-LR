ce68ae4cb40ae2019eabdc431d07333a113b32e7
steve-cse <stevebobygeorge@gmail.com>
2023-01-23 15:25:54 +0530

    Add flask app

--- deploy.py
+++ deploy.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask,render_template,request,send_from_directory
 import cloudpickle as cp
+import numpy as np
 from urllib.request import urlopen
 
 app=Flask(__name__)
-model=cp.load(urlopen('https://raw.githubusercontent.com/steve-cse/Spam-Filtering-BN/master/SFBN.pkl'))
+model=cp.load(urlopen('https://raw.githubusercontent.com/steve-cse/Loan-Eligibility-LR/master/logistic_model.pkl'))
 
 @app.route("/favicon.ico")
 def favicon():
@@ -16,13 +17,21 @@ def home():
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
-    new_email = request.form['email_input']
-    new_features = cv.transform([new_email])
-    prediction = model.predict(new_features)
+    new_credit = request.form['credit_input']
+    new_education = request.form['education_input']
+    new_gender = request.form['gender_input']
+
+    new_credit=float(new_credit)
+    new_gender = 1 if new_gender == "Male" else 0 if new_gender == "Female" else new_gender
+    new_education = 1 if new_education == "Not Graduate" else 0 if new_education == "Graduate" else new_education
+
+    features=[]
+    features+=[new_credit,new_education,new_gender]
+    prediction = model.predict(np.array(features).reshape(1, -1))
     if prediction[0]==1:
-        result="Spam Detected"
+        result="Eligible for Loan"
     else:
-        result= "Ham Detected"
+        result= "Not Eligible for Loan"
     return render_template('index.html', **locals())
 
 if __name__=='__main__':
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pickle as pkl
 import numpy as np
 model = pkl.load(open('logistic_model.pkl', 'rb')) # rb means read as binary
-Credit_History = 1
-Education = 0
+Credit_History = 0
+Education = 1
 Gender = 0
 lst=[]
 lst+=[Credit_History,Education,Gender]
